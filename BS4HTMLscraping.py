502440322ce22f7f6fc38a97c9a36b337708ad9d
nINi-spaghetti <nirani@tutamail.com>
2025-04-08 01:53:35 -0500

    Add files via upload

---
+++ BS4HTMLscraping.py
@@ -0,0 +1,101 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+"""
+Deep dive into Beautiful Soup for parsing HTML and XML.
+     * Practice exercises:
+       * Extracting titles, headings, and paragraphs from web pages.
+       * Scraping tables and lists.
+       * Handling different HTML structures.
+       * Learning to find specific elements using CSS selectors and Xpath.
+     * Debugging and error handling in web scraping.
+"""
+#https://www.crummy.com/software/BeautifulSoup/bs4/doc/
+"""html_doc = <html><head><title>The Dormouse's story</title></head>
+<body>
+<p class="title"><b>The Dormouse's story</b></p>
+
+<p class="story">Once upon a time there were three little sisters; and their names were
+<a href="http://example.com/elsie" class="sister" id="link1">Elsie</a>,
+<a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and
+<a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>;
+and they lived at the bottom of a well.</p>
+
+<p class="story">...</p>
+"""
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://repniemerg.com/about/'
+page = requests.get(url)
+soup = BeautifulSoup(page.text, 'html.parser')
+
+#print(soup.prettify())
+# <html>
+#  <head>
+#   <title>
+#    The Dormouse's story
+#   </title>
+#  </head>
+#  <body>
+#   <p class="title">
+#    <b>
+#     The Dormouse's story
+#    </b>
+#   </p>
+#   <p class="story">
+#    Once upon a time there were three little sisters; and their names were
+#    <a class="sister" href="http://example.com/elsie" id="link1">
+#     Elsie
+#    </a>
+#    ,
+#    <a class="sister" href="http://example.com/lacie" id="link2">
+#     Lacie
+#    </a>
+#    and
+#    <a class="sister" href="http://example.com/tillie" id="link3">
+#     Tillie
+#    </a>
+#    ; and they lived at the bottom of a well.
+#   </p>
+#   <p class="story">
+#    ...
+#   </p>
+#  </body>
+# </html>
+
+#Here are some simple ways to navigate that data structure:
+
+"""
+soup.title
+# <title>The Dormouse's story</title>
+
+soup.title.name
+# u'title'
+
+soup.title.string
+# u'The Dormouse's story'
+
+soup.title.parent.name
+# u'head'
+
+soup.p
+# <p class="title"><b>The Dormouse's story</b></p>
+
+#soup.p['class']
+# u'title'
+
+soup.a
+# <a class="sister" href="http://example.com/elsie" id="link1">Elsie</a>
+
+soup.find_all('p')
+# [<a class="sister" href="http://example.com/elsie" id="link1">Elsie</a>,
+#  <a class="sister" href="http://example.com/lacie" id="link2">Lacie</a>,
+#  <a class="sister" href="http://example.com/tillie" id="link3">Tillie</a>]
+
+#soup.find(id="link3")
+# <a class="sister" href="http://example.com/tillie" id="link3">Tillie</a>
+"""
+for a in soup.find_all('a', href=True):
+    print("Found the URL:", a['href'])
