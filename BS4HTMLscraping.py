f4a580974e208d3e6dce91f83a085555b3f89580
nini <nirani@tutamail.com>
2025-04-08 02:21:06 -0500

    Update BS4HTMLscraping.py

--- BS4HTMLscraping.py
+++ BS4HTMLscraping.py
@@ -1,7 +1,4 @@
 # This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 """
 Deep dive into Beautiful Soup for parsing HTML and XML.
      * Practice exercises:
